e918313fd65cbafc5f4bdee62bb527555a075ecd
JosueGarcia04 <josueadrian042006@gmail.com>
2024-07-09 08:56:59 -0600

    delete

--- todolist/app/urls.py
+++ todolist/app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns =[
     path("", views.index, name="index" ), 
     path("insert/", views.insert, name="insert"),
+    path("delete/<int:task_id>/", views.delete, name="delete")
 ]
--- todolist/app/views.py
+++ todolist/app/views.py
@@ -1,20 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Task
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 
 # Create your views here.
-def index(request):     
+def index(request):
     database = Task.objects.all()
     context = {
-        "database": database
+        "database": database[::-1]
     }
-
     return render(request, "app/index.html", context)
 
 def insert(request):
-    task_subject = request.POST["subject"]
-    task_description = request.POST["description"]
-    database = Task(subject=task_subject, description=task_description)
-    database.save()
+    try:
+        task_subject = request.POST["subject"]
+        task_description = request.POST["description"]
+        if task_subject == "" or task_description == "":
+            raise ValueError("El texto no puede estar vacío :(")
+        database = Task(subject=task_subject, description=task_description)
+        database.save()
+        return HttpResponseRedirect(reverse("index"))
+    except ValueError as err:
+        print(err)
+        return HttpResponseRedirect(reverse("index"))
+
+def delete(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    task.delete()
     return HttpResponseRedirect(reverse("index"))
