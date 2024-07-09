d99828234d72a56d1505c418ad38cf5a2e6db0c3
JosueGarcia04 <josueadrian042006@gmail.com>
2024-07-08 23:36:14 -0600

    adding to crud records from database

--- todolist/app/views.py
+++ todolist/app/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
+from .models import Task
+from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    context = {}
+    database = Task.objects.all()
+    context = {
+        "database": database
+    }
+    print(database)
     return render(request, "app/index.html", context)
+
+def insert(request):
+    return HttpResponse("hi django")
