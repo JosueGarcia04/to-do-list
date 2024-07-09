f82b1e190e57c376a89899acf38ae94a4c3f73c7
JosueGarcia04 <josueadrian042006@gmail.com>
2024-07-08 23:50:13 -0600

    saving tasks from form

--- todolist/app/views.py
+++ todolist/app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Task
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 def index(request):
@@ -8,8 +9,12 @@ def index(request):
     context = {
         "database": database
     }
-    print(database)
+
     return render(request, "app/index.html", context)
 
 def insert(request):
-    return HttpResponse("hi django")
+    task_subject = request.POST["subject"]
+    task_description = request.POST["description"]
+    database = Task(subject=task_subject, description=task_description)
+    database.save()
+    return HttpResponseRedirect(reverse("index"))
